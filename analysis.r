aea76c929a5022840913c99c43241a52e36be573
lbsell <102692443+lbsell@users.noreply.github.com>
2022-05-23 23:01:40 -0700

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -1,1 +1,6 @@
-** Info 201 - A3 - Data Visualization - Luke Sellers
+**"Info 201 - A3 - Data Visualization - Luke Sellers"
+
+*"Introduction"
+
+Systemic injustice and racism have deep historical roots in the United States of America. 
+The intetent of this problem domain is to observe the overall trends 
